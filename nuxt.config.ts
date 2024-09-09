e6b3bc08b3a6aff88960559df0832fbb503101f1
unknown <bensonagala@live.com>
2024-09-09 09:17:33 +0300

    cleanup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt",]
-  
-  
-  
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt",],
+  compatibilityDate: "2024-09-06",
 }
 )

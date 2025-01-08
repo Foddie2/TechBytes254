0e2248181f62e08cc5d8dc79bc565684f46aceef
Foddie2 <bensonagala@live.com>
2025-01-08 16:25:10 +0300

    SEO setup

--- app.vue
+++ app.vue
@@ -10,10 +10,10 @@
 
 <script setup lang="ts">
 useSeoMeta({
-  title: 'My Amazing Site',
-  ogTitle: 'My Amazing Site',
-  description: 'This is my amazing site, let me tell you all about it.',
-  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  title: 'Techbytes254',
+  ogTitle: 'Techbytes254',
+  description: 'This is Techbytes254, let me tell you all about it.',
+  ogDescription: 'This is Techbytes254, let me tell you all about it.',
   ogImage: 'https://example.com/image.png',
   twitterCard: 'summary_large_image',
 })

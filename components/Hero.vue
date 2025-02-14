86637feb04bfa9bc513294b5f1f905d3ba8d2150
Foddie2 <bensonagala@live.com>
2025-02-14 12:05:06 +0300

    Hero bug fix

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,9 +2,9 @@
 
 
     <section
-      class="relative bg-no-repeat">
+      class="relative bg-no-repeat ">
       <div
-        class="absolute inset-0 bg-blue/75 sm:bg-transparent sm:from-blue/95 sm:to-white/25 ltr:sm:bg-gradient-to-r bg-gradient-to-r from-blue-400 to-teal-400"
+        class="absolute h-screen inset-0 bg-blue/75 sm:bg-transparent sm:from-blue/95 sm:to-white/25 ltr:sm:bg-gradient-to-r bg-gradient-to-r from-blue-400 to-teal-400"
       ></div>
 
      

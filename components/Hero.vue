491b1b5b5fad087683449a93b9a3d8da299afdcc
Foddie2 <bensonagala@live.com>
2025-03-04 13:46:44 +0300

    Pages updates

--- components/Hero.vue
+++ components/Hero.vue
@@ -16,8 +16,8 @@
                   <h1 class="text-4xl sm:text-4xl md:text-5xl lg:text-7xl font-extrabold uppercase leading-tight tracking-tight">
                     Empowering Businesses through <span class="text-stone-900">Technology</span>
                   </h1>
-                  <p class="mt-4 text-lg sm:text-lg md:text-xl font-medium dark:text-gray-400 text-gray-700 text-balance" >
-                    Where imagination meets innovation, We do everything with you in mind! We help you grow your business.
+                  <p class="mt-4 text-lg sm:text-lg md:text-xl lg:text-2xl font-medium dark:text-gray-400 text-gray-700 text-balance" >
+                    We do everything with you in mind! We help you grow your business.
                   </p>
                   <div class="mt-6 sm:mt-8 flex flex-wrap gap-4">
                     <NuxtLink to="/about">

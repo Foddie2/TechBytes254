a4b31f2bda2a518d11228668fac04b0091cd5e9e
Foddie2 <bensonagala@live.com>
2025-03-19 18:14:48 +0300

    Working Form updates

--- components/Contact.vue
+++ components/Contact.vue
@@ -88,7 +88,7 @@
 <script setup>
   const form = ref({
     access_key: "07298aa2-112b-4476-9933-227a0f8f980a",
-    subject: "New Submission from Web3Forms",
+    subject: "New message sent from Techbytes",
     name: "",
     email: "",
     message: "",
